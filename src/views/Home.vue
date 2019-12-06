f660ae927d86eac80efa45e70dd8a7783a6e89a3
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-12-05 17:59:13 -0700

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>PokeDex</h1>
+    <form>
+      <input type="number" v-model="pokemonId" />
+      <button @click="getAllPokemon">Check Pokedex</button>
+    </form>
+    <h2>{{ pokemonTitle }} {{ pokemonId }}</h2>
+    <img :alt="pokemonName" :src="pokemonImage" />
+    <HelloWorld :msg="pokemonTitle" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      pokemonId: 1,
+      pokemonName: "",
+      pokemonImage: ""
+    };
+  },
+  computed: {
+    pokemonTitle() {
+      return "Pokemon: " + this.pokemonName;
+    }
+  },
+  mounted() {
+    this.getAllPokemon();
+  },
+  methods: {
+    async getAllPokemon() {
+      const { data, status } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`
+      );
+      if (status === 200) {
+        this.pokemonName = data.name;
+        this.pokemonImage = data.sprites.front_default;
+        // eslint-disable-next-line
+        console.log(this.pokemonName, this.pokemonImage);
+      } else {
+        // eslint-disable-next-line
+        console.log("didnt work yo");
+      }
+    }
   }
 };
 </script>
